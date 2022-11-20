a1b08bee108f5811ef68c696caa93b20e2ba6f0b
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-11-20 18:55:41 +0700

    hanlde random ID

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,20 @@ import './App.css';
 
 function App() {
   const [cats, setCats] = useState([]);
+  const [id, setId] = useState([]);
+  const [cat, setCat] = useState();
+  const [randomId, setRandomId] = useState(``);
   const api_key = 'live_VWyv5EWKyqzuJylVyL1u7DrA6zXGgiS9ZItpD0yJvLWmq9SbRGLIyxqrLhDlfHIE';
   const url_cats = `https://api.thecatapi.com/v1/breeds`;
 
   const getDataCats = () => {
     axios.get(url_cats)
-      // .then(res => setCats(res.data))
-      // .then(res => console.log(res))
+      .then(res => res.data.map(item => {
+        setId(state => {
+          state = [...state, item.image && item.image.id];
+          return state
+        })
+      }))
       .catch(err => console.log(err))
   };
 
@@ -18,14 +25,28 @@ function App() {
     getDataCats();
   }, []);
 
-  console.log('cats', cats);
+  // console.log('id', id);
 
-  const a = ['aku', 'kamu', 'dia'];
-  const randomA = Math.floor(Math.random()*a.length);
-  console.log('tes', a[randomA])
+  //NGERANDOM
+  // const a = ['aku', 'kamu', 'dia'];
+  // const randomA = Math.floor(Math.random()*a.length);
+  // console.log('tes', a[randomA])
 
+  const handleRandomId = () => {
+    const a = Math.floor(Math.random()*id.length);
+    setRandomId(id[a]);
+  };
 
+  const getDataCat = () => {
+    axios.get(`https://api.thecatapi.com/v1/images/EPF2ejNS0`)
+      .then(res => console.log(res))
+  }
 
+  const handleCat = () => {
+    getDataCat();
+  };
+
+  console.log(randomId)
 
   return (
     <div className="App">
@@ -33,11 +54,15 @@ function App() {
       {/* {
         cats.map(cat => (
           <>
-            <img src={cat.url} style={{width: '150px'}} alt="" />
-            <p>{cat.breeds[0].name}</p>
+            <img src={cat.image && cat.image.url} style={{width: '150px'}} alt="" />
+            <p>{cat.name}</p>
           </>
         ))
       } */}
+      <button onClick={() => {
+        // handleCat();
+        handleRandomId()
+        }}>ClikMe</button>
     </div>
   );
 }
